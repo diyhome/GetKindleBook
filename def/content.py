1f5623b9a54a80b31b3a0e6e1847c13b291234bc
diyhome <2363400784@qq.com>
2019-03-11 15:10:46 +0800

    Coding easy get

--- def/content.py
+++ def/content.py
@@ -1,2 +1,15 @@
-class content():
-    def __init__(self):
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+
+_features = "html.parser"
+
+class _Content():
+    def __init__(self,index_url,reg):
+        self.in_url = index_url
+        self.rul = reg
+    
+    def By_class(self):
+        html = urllib.request.urlopen(self.in_url)
+        csoup = BeautifulSoup(html,_features)
+        for tmp in csoup.find_all(_class=self.rul)
