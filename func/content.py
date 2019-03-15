b0c493c72ba5953c1d9ca9215889be7f81241425
diyhome <2363400784@qq.com>
2019-03-15 14:38:37 +0800

    rewrite func/content.py by try on request web

--- func/content.py
+++ func/content.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import re
 import urllib
-import urllib.request
 
+from urllib import request
+from urllib import error
 from bs4 import BeautifulSoup
 
 _features = "lxml"
@@ -12,49 +13,43 @@ class Content:
     tit="null"
     def __init__(self):
         print("Runing Content class...")
-        self.page = "Loading..."
-        self.content = "Loading..."
-    
+
     def GUrl(self,url,sre):
         linkData = []
-        html = urllib.request.urlopen(url)
+        html = self.htmlcode(url)
         csoup = BeautifulSoup(html,_features)
-        self.page = csoup.prettify()
-        #  print(csoup.prettify())
         url_list = csoup.find_all(href=re.compile(sre))
-        #  print(url_list)
         for tmp in url_list:
             t = tmp.get('href')
-            #  print(t)
             if t.startswith('http://'):
                 linkData.append(t)
             else:
                 linkData.append(url+t)
         return linkData
-    def GText(self,urls,ic,sre):
-        Text = ""
-        tit = ""
-        html = urllib.request.urlopen(urls)
+    def GText(self,url,ic,sre):
+        tit = None
+        html = self.htmlcode(url)
         tsoup = BeautifulSoup(html,_features)
         self.tit = tsoup.title.string
         self.content = tsoup.prettify()
-        #  Text = tsoup.find(class_=sre)
         if ic == "class_":
             Text = tsoup.find(class_=sre)
-            #  print(sre)
-            #  print("Runing GText class")
         else:
             Text = tsoup.find(id=sre)
         if Text is None:
-               print("Content is null.Faild:"+urls)
-               return
+            print("Content is null.Faild:"+url)
+            return
         else:
-            #  Text = Text.get_text()
             return Text.get_text()
-    def ts(self,url):
-        html = urllib.request.urlopen(url)
-        s = BeautifulSoup(html,_features)
-        print(s.prettify())
+    def htmlcode(self,url):
+        headers = {"User-Agent":" Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"}
+        req = request.Request(url=url,headers=headers)
+        try:
+            response = request.urlopen(req)
+            html = response.read()
+            return html
+        except error.URLError as e:
+            print(e.reason)
 
 if __name__ == "__main__":
     print("你可以通过编辑main函数测试!")
