137a9bc3c6fc3dd7ecdad682c2ca2af33bd93c5d
diyhome <diyhome@outlook.com>
2019-06-19 11:04:46 +0800

    code optimization

---
+++ func/WebPage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    WebPage
+    ~~~~ on 六月 19,2019 10:56
+    
+    The description is written here.
+    Author: diyhome
+    :license MIT, see LICENSE for more details.
+    :copyright (c) 2019 by diyhome<diyhome@outlook.com>.
+"""
