b853622305e84b08e4d4504f80c136c9cf4edf0f
jinming <diyhome@outlook.com>
2019-06-21 15:31:34 +0800

    漏传了一条

---
+++ GetKindleBook/spiders/banzhu.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.selector import Selector
+from GetKindleBook.items import EbookItem
+
+class BanzhuSpider(scrapy.Spider):
+    name = 'banzhu'
+    allowed_domains = ['banzhuer.com']
+    start_urls = ['https://www.banzhuer.com/6_6685/']
+    custom_settings = {
+        "DOWNLOAD_DELAY": 0.05,
+        # "CONCURRENT_REQUESTS_PER_DOMAIN": 2
+    }
+    def __init__(self, links=None, *args, **kwargs):
+        super(BanzhuSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [links]
+
+    def parse(self, response):
+        selsector = Selector(response)
+        print(response)
+        find_all = selsector.xpath('//*[@id="list"]/dl/dd[position()>9]/a')   #这网站有毒,前九章是最新更新的,避免选择前九章
+        num = 0
+        for section in find_all:
+            num += 1
+            href = section.xpath('.//@href').extract_first()
+            UrlChate = response.urljoin(href)
+            request = scrapy.Request(UrlChate, callback=self.parse_detail, meta={'num': num})
+            yield request
+
+    def parse_detail(self, response):
+        xzbq = Selector(response)
+        huan = xzbq.xpath('//*[@id="content"]/text()').extract()
+        conn = xzbq.css('div.content_read > div > div.bookname > h1::text').extract_first()
+        # cont = "\n".join(huan) #{'a','b','c'} -> a-b-c
+        item = EbookItem()
+        item['num'] = response.meta['num']
+        item['ChaterNa'] = conn
+        item['ChaterCo'] = huan
+        yield item
